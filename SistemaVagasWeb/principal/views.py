9a5a22871ca95b1b26f82486122eb0cc0b493f55
Renato <lacerdarenato00@gmail.com>
2021-06-09 13:01:26 -0300

    Criar folha de estilos principal

--- SistemaVagasWeb/principal/views.py
+++ SistemaVagasWeb/principal/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('olá mundo')
+    return render(request, 'principal/index.html')
